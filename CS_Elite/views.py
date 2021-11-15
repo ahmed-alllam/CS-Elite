cec9e767d4b26d165fa315dcae6a8f2a31f1ae19
Ahmed Emad <ahmed39652003@gmail.com>
2021-11-15 13:19:38 +0200

    Added Pager to Articles

--- CS_Elite/views.py
+++ CS_Elite/views.py
@@ -1,17 +1,34 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from articles import models
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'index.html'
+    model = models.Article
+    paginate_by = 2
+    context_object_name = 'articles'
+    pages_range = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['articles'] = models.Article.objects.filter()
         context['categories'] = models.Category.objects.filter()
         context['top_categories'] = models.Category.objects.filter()[0:5]
-        return context
+        context.update(self.get_paginator_context(context.get('page_obj')))
 
-class ArticlesByCategory(TemplateView):
+        return context
+    
+    def get_paginator_context(self, page_obj):
+        return {
+            'previous_pages_range': range(max(1, page_obj.number - self.pages_range), 
+                                    page_obj.number),
+            'next_pages_range': range(page_obj.number + 1, 
+                                    min(page_obj.paginator.num_pages + 1, 
+                                        page_obj.number + self.pages_range + 1))
+        }
+
+class ArticlesByCategory(ListView):
     template_name = 'articles_list.html'
+    model = models.Article
+    paginate_by = 20
+    context_object_name = 'articles'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
