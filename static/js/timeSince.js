6f8e5b3676e178bdf1d491654d404ce3da928f43
Ahmed Emad <ahmed39652003@gmail.com>
2021-11-20 13:05:01 +0200

    Added DateTime Parsing

---
+++ static/js/timeSince.js
@@ -0,0 +1,33 @@
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+  
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    return "a minute ago";
+}
+
+var timeElems = document.body.getElementsByTagName("time");
+
+for(time of timeElems) {
+  time.innerHTML = timeSince(new Date(time.dateTime * 1000));
+}
