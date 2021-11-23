99e27d4a29c2898089b0fede070b5502998ac0b7
Ahmed Emad <ahmed39652003@gmail.com>
2021-11-23 10:15:19 +0200

    Added Nav header on scrollevent to chage arrow buttons visibilty

--- articles/static/js/headerScrollButtons.js
+++ articles/static/js/headerScrollButtons.js
@@ -4,9 +4,28 @@ const previousButton = document.getElementById('previous');
 const list = document.getElementById('categories-list');
 
 nextButton.onclick = function () {
-    list.scrollLeft = list.scrollWidth - list.clientWidth;
+    list.scrollLeft = list.scrollWidth - list.offsetWidth;
+
+    nextButton.style.visibility = "hidden";
+    previousButton.style.visibility = "visible";
 };
 
 previousButton.onclick = function () {
     list.scrollLeft = 0;
+
+    previousButton.style.visibility = "hidden";
+    nextButton.style.visibility = "visible";   
 };
+
+list.onscroll = () => {
+    if ((list.scrollLeft + list.offsetWidth) >= list.scrollWidth){
+        nextButton.style.visibility = "hidden";
+        previousButton.style.visibility = "visible";
+    } else if (list.scrollLeft <= 0) {
+        previousButton.style.visibility = "hidden";
+        nextButton.style.visibility = "visible";
+    } else {
+        previousButton.style.visibility = "visible";
+        nextButton.style.visibility = "visible";
+    }
+}
